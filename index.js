8f795be921ce039afc288fb497875de6bf46ffeb
DedSec002 <pandianhelp@gmail.com>
2020-12-25 20:54:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,74 @@
+// const app = require('express')()
+// const http = require('http').createServer(app)
+// const io = require('socket.io')(http);
+
+// app.get('/', (req, res) => {
+
+//   io.clients((error, clients) => {
+//     if (error) throw error;
+//     res.send(clients); // => [6em3d4TJP8Et9EMNAAAA, G5p55dHhGgUnLUctAAAB]
+//   });
+
+// })
+
+
+
+// io.on('connection', socket => {
+//   //Get the chatID of the user and join in a room of the same name
+//   chatID = socket.handshake.query.chatID
+//   tokenID = socket.handshake.query.tokenID
+//   socket.join(chatID)
+
+
+
+//   //Leave the room if the user closes the socket
+//   socket.on('disconnect', () => {
+//     socket.leave(chatID)
+//   })
+
+//   socket.clients((clients) => {
+//     socket.broadcast.emit("count", clients);
+//   });
+
+
+//   //Send message to only a particular user
+//   socket.on('send_message', message => {
+//     receiverChatID = message.receiverChatID
+//     senderChatID = message.senderChatID
+//     senderNameID = message.senderNameID
+//     content = message.content
+//     iv = message.iv
+
+//     //Send message to only that particular id
+//     socket.in(receiverChatID).emit('receive_message', {
+//       'content': content,
+//       'senderChatID': senderChatID,
+//       'senderNameID': senderNameID,
+//       'receiverChatID': receiverChatID,
+//       'tokenID': tokenID,
+//       'iv': iv
+//     })
+//   })
+// });
+
+// http.listen(process.env.PORT)
+
+
 const app = require('express')()
 const http = require('http').createServer(app)
-const io = require('socket.io')(http);
+
 
 app.get('/', (req, res) => {
-
-  io.clients((error, clients) => {
-    if (error) throw error;
-    res.send(clients); // => [6em3d4TJP8Et9EMNAAAA, G5p55dHhGgUnLUctAAAB]
-  });
-
+    res.send("Node Server is running. Yay!!")
 })
 
+//Socket Logic
+const socketio = require('socket.io')(http)
 
-
-io.on('connection', socket => {
-  //Get the chatID of the user and join in a room of the same name
-  chatID = socket.handshake.query.chatID
-  tokenID = socket.handshake.query.tokenID
-  socket.join(chatID)
-
-
-
-  //Leave the room if the user closes the socket
-  socket.on('disconnect', () => {
-    socket.leave(chatID)
-  })
-
-  socket.clients((clients) => {
-    socket.broadcast.emit("count", clients);
-  });
-
-
-  //Send message to only a particular user
-  socket.on('send_message', message => {
-    receiverChatID = message.receiverChatID
-    senderChatID = message.senderChatID
-    senderNameID = message.senderNameID
-    content = message.content
-    iv = message.iv
-
-    //Send message to only that particular id
-    socket.in(receiverChatID).emit('receive_message', {
-      'content': content,
-      'senderChatID': senderChatID,
-      'senderNameID': senderNameID,
-      'receiverChatID': receiverChatID,
-      'tokenID': tokenID,
-      'iv': iv
+socketio.on("connection", (userSocket) => {
+    userSocket.on("send_message", (data) => {
+        userSocket.broadcast.emit("receive_message", data)
     })
-  })
-});
+})
 
 http.listen(process.env.PORT)
